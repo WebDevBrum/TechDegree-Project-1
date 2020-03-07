2cb466063842df7f1dc328d2c4fc120cb7d46c7e
Scott Davies <57508310+theDevelopingDeveloper@users.noreply.github.com>
2020-03-07 06:21:40 +0000

    Added quotes Array
    A number of quotes from Stephen King novels.

--- js/script.js
+++ js/script.js
@@ -10,7 +10,44 @@ project 1 - A Random Quote Generator
 /*** 
  * `quotes` array 
 ***/
-
+let quotes = [
+  {
+    quote: 'Monsters are real, Ghosts are too. They live inside of us and sometimes they win.',
+    source: 'Stephen King',
+    citation: 'The Shining',
+    year: '1977'
+  }, 
+  {
+    quote: 'People who try hard to do the right thing always seem mad.',
+    source: 'Stephen King',
+    citation: 'The Stand',
+    year: '1978'
+  },
+  {
+    quote: 'Time takes it all. Whether you want it to or not',
+    source: 'Stephen King',
+    citation: 'The Green Mile',
+    year: '1996'
+  },
+  {
+    quote: 'We lie best when we lie to ourselves',
+    source: 'Stephen King',
+    citation: 'It',
+    year: '1986'
+  },
+  {
+    quote: "There's no harm in hoping for the best as long as you're prepared for the worst.",
+    source: 'Stephen King',
+    citation: 'Rita Hayworth and Shawshank Redemption',
+    year: '1982'
+  },
+  {
+    quote: "If you don't control your temper, your temper will control you",
+    source: 'Stephen King',
+    citation: 'Under The Dome',
+    year: '2009'
+  }
+];
 
 
 /***
